4f99355784bcd35920d484bd52e57343eb80a008
Daniel Makover <DanielM@centiro.se>
2021-10-12 02:48:39 +0200

    boiler plate code: random question generator

--- app/app.py
+++ app/app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, request,redirect
 from flask_sqlalchemy import SQLAlchemy
-
+import random
 
 #create Flask opbject
 app = Flask(__name__)
@@ -14,6 +14,7 @@ db = SQLAlchemy(app)
 
 from models import Question
 
+   
 #ROUTES
 @app.route('/')
 def main():
@@ -21,10 +22,25 @@ def main():
 
 @app.route('/game')
 def game():
-	question = Question.query.all()
-	print(question)
-	return render_template('game.html')
+    #get the total number of questions
+    total_num_questions = Question.query.count()
+    #select a randon number annd conver to int
+    random_question_number = random.randint(1,total_num_questions)
+    random_question_number = str(random_question_number)
+    
+    #select a random question
+    q = Question.query.get(random_question_number)
+    return render_template('game.html' , q = q, num =total_num_questions )
 
+@app.route('/question/<int:id>')
+def getSingleQuestion(id):  
+    str_id = str(id)                      
+    q = Question.query.get(str_id)
+    print(q.question)
+    return (str(q.question))
+    	
 @app.route('/leaderboard')
 def leaderBoard():
 	return render_template('leaderboard.html')
+
+
