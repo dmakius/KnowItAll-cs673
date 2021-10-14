cbcbb1683c04093e1cf479f9f316becabb4404c2
mauricerr <mauricehyz@gmail.com>
2021-10-14 11:52:05 -0400

    Update game.js

--- app/static/js/game.js
+++ app/static/js/game.js
@@ -8,10 +8,9 @@ $(document).ready(function () {
     //keep answer location
     var answer_location;
     //keep user selection
-    var user_selection;
+    var user_selection = [];
     // count how many attempt used
     var attempt_counter = 0;
-
     //Make Add score to Leader board form appear
 
     $('#submitScore').click(function () {
@@ -20,26 +19,29 @@ $(document).ready(function () {
     });
 
     // clicking on any of the options and buttons
-    $('.option').click(function () {
-        //var clicked = $(this).attr("clicked");
+    var opt = $('.option');
+    opt.on('click',Selection);
+    function Selection() {
         //get user selection
         user_selection = $(this);
-        console.log('clicked!');
-        //TODO: subtract lives if neccesary
-        //TODO: Assign points
-    });
+        console.log(user_selection[0]+'clicked!');
+    }
+
+    //TODO: subtract lives if neccesary
+    //TODO: Assign points
+
     //Display new question
     $('#next').on('click', DisplayNewQuestion);
     //Check if selection is correct
-    $('#submit').on('click', Submit);
+    var sub = $('#submit');
+    sub.on('click', Submit);
     //------------------------------------
     //SELECT A NEW RANDOM QUESTION
     //TODO: Replace max number with number of questions come backend
     function DisplayNewQuestion() {
-
+        sub.on('click', Submit);
         // replace the changed color back.
-        $(Answer()).css('color', 'black')
-        $(user_selection).css('color', 'black')
+        $('.option').css('color', 'black')
         var max_number = 50;
         var test_int = Math.floor(Math.random() * max_number);
         $.ajax({
@@ -53,31 +55,33 @@ $(document).ready(function () {
 
                 //replace front end ui with NEW data from server
                 $('#question').text(data[1]['Question']);
-                $('#option_1').text("A: " + data[2]['Answer']);
-                option1 = data[2]['Answer'];
-                $('#option_2').text("B: " + data[3]['Option_1']);
-                option2 = data[3]['Option_1'];
-                $('#option_3').text("C: " + data[4]['Option_2']);
-                option3 = data[4]['Option_2'];
-                $('#option_4').text("D: " + data[5]['Option_3']);
-                option4 = data[5]['Option_3'];
+                option1 = $('#option_1').text("A: " + data[2]['Answer']);
+
+                option2 = $('#option_2').text("B: " + data[3]['Option_1']);
+
+                option3 = $('#option_3').text("C: " + data[4]['Option_2']);
+
+                option4 = $('#option_4').text("D: " + data[5]['Option_3']);
+
                 answer_location = data[6]['Answer_Location'];
             }
         });
     }
 
-    //function UserSelect(){
-    //    user_selection = this.id
-    //    return user_selection
-    //}
-
-    // function for submit button and check attempts
+    // return answer, count attempts
     function Submit() {
 
-        if (user_selection == Answer()) {
+        sub.off('click', Submit);
+
+        //console.log(user_selection);
+        //console.log(Answer());
+        if (user_selection[0] == Answer()) {
             // change answer color to green
             $(Answer()).css('color', 'green');
-            // score function people can add their score++ in this if function
+
+
+            // score function can be added score++ in this if function
+
         } else {
             // change user_selection color to red, and answer to green
             $(user_selection).css('color', 'red');
@@ -85,10 +89,13 @@ $(document).ready(function () {
 
             // attempt counter count one chance
             attempt_counter++;
-            // score function people can add their score-- in here.
+
+
+            // score function can be added score-- in here.
+
         }
 
-        // when 3 attempts, game over, goes to the leader board.
+        // when 3 attempts, game over, only allow to submit score.
         if (attempt_counter >= 3) {
             alert("GameOver!" + "Score shows up here");
             $('#next').detach();
