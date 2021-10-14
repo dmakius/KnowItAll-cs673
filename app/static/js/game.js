c1e909233cd39ee25db7de483c581e69a11c165d
fishabel <abelwu@bu.edu>
2021-10-14 13:50:26 -0400

    Skip Question button
    Up to 3 times in a single game session

--- app/static/js/game.js
+++ app/static/js/game.js
@@ -5,6 +5,35 @@ $(document).ready(function () {
         console.log('clicked!');
         $('#cover-caption').slideToggle("slow");
     });
+    const MaxSkip = 3
+    var skipnum = 0
+    $('#SkipQuestion').click(function(){
+        if (skipnum >= 3){
+            console.log('No more skips!');
+            return false;
+        }
+        skipnum += 1
+        console.log('Skipped!');
+        var max_number = 50;
+        var test_int = Math.floor(Math.random() * max_number);
+        $.ajax({
+            dataType: 'json',
+            type: 'GET',
+            url: '/question/' + test_int,
+            success: function (data) {
+                console.log(typeof data);
+                console.log(data);
+                $('#question').text(data[1]['Question']);
+                $('#option_1').text("A: " + data[2]['Answer']);
+                $('#option_2').text("B: " + data[3]['Option_1']);
+                $('#option_3').text("C: " + data[4]['Option_2']);
+                $('#option_4').text("D: " + data[5]['Option_3']);
+            }
+        });
+        if (skipnum >= MaxSkip){
+            document.getElementById('SkipQuestion').disabled = true;
+        };
+    });
 
     // clicking on any of the options
     $('.option').click(function () {
