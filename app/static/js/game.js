d9c15bd34d7b992e52c85a884d652c21c4df3002
mauricerr <mauricehyz@gmail.com>
2021-10-14 19:14:07 -0400

    update
    -next question not shows up before click submit button
    -remaining lives always on
    -bug fix: the first question now is randomized and will return answer_location

--- app/static/js/game.js
+++ app/static/js/game.js
@@ -11,39 +11,53 @@ $(document).ready(function () {
     var user_selection = [];
     // count how many attempt used
     var attempt_counter = 3;
-    //Make Add score to Leader board form appear
 
+
+
+    //Make Add score to Leader board form appear
     $('#submitScore').click(function () {
         console.log('clicked!');
         $('#cover-caption').slideToggle("slow");
     });
 
+
     // clicking on any of the options and buttons
     var opt = $('.option');
-    opt.on('click',Selection);
+    opt.on('click', Selection);
+
     function Selection() {
         //get user selection
         user_selection = $(this);
-        console.log(user_selection[0]+'clicked!');
+        console.log(user_selection[0] + 'clicked!');
     }
 
     //TODO: subtract lives if neccesary
+    //Display remaining lives
     $(".stats").show();
+    DisplayStats();
     //TODO: Assign points
 
-    //Display new question
+    //Next button, for new question
     var next = $('#next');
     next.off('click', DisplayNewQuestion);
-    //Check if selection is correct
+    //Hide next button before click submit
+    next.hide();
+
+    //Submit button, Check if selection is correct
     var sub = $('#submit');
     sub.on('click', Submit);
+    
+    // Load random question when load the page
+    DisplayNewQuestion();
+
     //------------------------------------
     //SELECT A NEW RANDOM QUESTION
     //TODO: Replace max number with number of questions come backend
     function DisplayNewQuestion() {
         // turn on submit button, new question, user can do submit
         sub.on('click', Submit);
-        // turn off next button, user cannot go next before submit
+        // turn off and hide next button, user cannot go next before submit
+        next.hide()
         next.off('click', DisplayNewQuestion);
         // replace the changed color back.
         $('.option').css('color', 'black')
@@ -61,13 +75,9 @@ $(document).ready(function () {
                 //replace front end ui with NEW data from server
                 $('#question').text(data[1]['Question']);
                 option1 = $('#option_1').text("A: " + data[2]['Answer']);
-
                 option2 = $('#option_2').text("B: " + data[3]['Option_1']);
-
                 option3 = $('#option_3').text("C: " + data[4]['Option_2']);
-
                 option4 = $('#option_4').text("D: " + data[5]['Option_3']);
-
                 answer_location = data[6]['Answer_Location'];
             }
         });
@@ -78,10 +88,15 @@ $(document).ready(function () {
         // turn off Submit button, user already submitted once
         sub.off('click', Submit);
         // turn on next button
+        next.show();
         next.on('click', DisplayNewQuestion);
+
+        // just for checking
         console.log(answer_location);
         //console.log(user_selection);
         //console.log(Answer());
+
+        // if else function for check if player answer right or wrong
         if (user_selection[0] == Answer()) {
             // change answer color to green
             $(Answer()).css('color', 'green');
@@ -121,14 +136,14 @@ $(document).ready(function () {
         } else {//if (answer_location == 4){
             q_answer = option_4;
         } //else {
-            //DisplayNewQuestion
+        //DisplayNewQuestion
         //}
         //console.log(q_answer);
         return q_answer;
     }
 
-    function DisplayStats(){
-    $(".stats").html('<h4>' + 'remaining lives: ' + attempt_counter +'/3' +'</h4>');
-}
+    function DisplayStats() {
+        $(".stats").html('<h4>' + 'remaining lives: ' + attempt_counter + '/3' + '</h4>');
+    }
 
 });
