09e55a45553ef34cba4399d2ec37eb8fb4d638ff
mauricerr <mauricehyz@gmail.com>
2021-10-15 13:42:16 -0400

    update with three tasks
    - The player is required to select an option; otherwise, when they click submit, an alert will pop out.
    
    - When the player clicks the option, the option background will turn grey. Once they click submit, the right answer background will turn green, and player selection text will turn red.
    
    - When the remaining lives come to zero, the post score page will directly pop-out, with both “post” button and “quit” button

--- app/static/js/game.js
+++ app/static/js/game.js
@@ -12,7 +12,6 @@ $(document).ready(function () {
     // count how many attempt used
     var attempt_counter = 3;
 
-
     //---------------------------------
     //CLICK/BUTTON
     //Make Add score to Leader board form appear
@@ -20,10 +19,14 @@ $(document).ready(function () {
         console.log('clicked!');
         $('#cover-caption').slideToggle("slow");
     });
+    $('#submitScore').hide()
 
-    // clicking on any of the options and buttons
+
     var opt = $('.option');
+    // clicking on any of the options will return value to user_selection
     opt.on('click', Selection);
+    // click option will change the option background color
+    opt.on('click', ChangeSelectedOptionColor);
 
     //Submit button, Check if selection is correct
     var sub = $('#submit');
@@ -46,6 +49,7 @@ $(document).ready(function () {
     //Display remaining lives
     $(".stats").show();
     DisplayStats();
+
     //TODO: Assign points
 
     //get user selection when player click the option
@@ -54,20 +58,33 @@ $(document).ready(function () {
         console.log(user_selection[0] + 'clicked!');
     }
 
+    // change the player selected option's color
+    function ChangeSelectedOptionColor() {
+        opt.css('background-color', 'white').css('color', 'black');
+        $(user_selection).css('background-color', 'grey').css('color', 'white');
+    }
+
     //TODO: subtract lives if neccesary
 
     //------------------------------------
     //SELECT A NEW RANDOM QUESTION
     //TODO: Replace max number with number of questions come backend
     function DisplayNewQuestion() {
+        // turn on the color change function for selected option
+        opt.on('click', ChangeSelectedOptionColor);
+
         // show submit button, new question, user can do submit
         sub.show();
 
         // turn off and hide next button, user cannot go next before submit
-        next.hide()
+        next.hide();
 
-        // replace the changed color back.
-        $('.option').css('color', 'black')
+        // replace the changed color back, and background color back.
+        opt.css('color', 'black');
+        opt.css('background-color', 'white').css('color', 'black');
+
+        // refresh user selection to an empty list
+        user_selection = [];
 
         var max_number = 50;
         var test_int = Math.floor(Math.random() * max_number);
@@ -93,58 +110,77 @@ $(document).ready(function () {
 
     // return answer, count attempts
     function Submit() {
-        // hide Submit button, user already submitted once
-        //sub.off('click', Submit);
-        sub.hide();
 
-        // show next button
-        next.show();
-        // if else function for check if player answer right or wrong
-        // user_selection is getting from function Selection(),
-        // and user_selection is a list, value locate at [0] is a HTML<div>...</div>
-        // something like this: <div class="col-md-5 text-center option btn btn-outline-secondary" id="option_2" style="color: green;">B: Baker Street</div>
-        // So that we can use user_selection[0] compare with the result from function CheckAnswer()
-        // to check if player selection is right or wrong
-        if (user_selection[0] == CheckAnswer()) {
-            // change answer color to green
-            $(CheckAnswer()).css('color', 'green');
+        // check user selection is empty or not
+        if (user_selection[0] == undefined){
 
-
-            // score function can be added score++ in this if function
+            alert("Please select an option!");
 
         } else {
-            // change user_selection color to red, and answer to green
-            $(user_selection).css('color', 'red');
-            $(CheckAnswer()).css('color', 'green');
+            // turn off the selected option color change function,
+            // when user submit their answer, selected option no longer available to change color
+            opt.off('click', ChangeSelectedOptionColor);
 
-            // attempt counter count one chance
-            attempt_counter--;
+            // hide Submit button, user already submitted once
+            sub.hide();
+
+            // show next button
+            next.show();
+            // if else function for check if player answer right or wrong
+            // user_selection is getting from function Selection(),
+            // and user_selection is a list, value locate at [0] is a HTML<div>...</div>
+            // something like this: <div class="col-md-5 text-center option btn btn-outline-secondary" id="option_2" style="color: green;">B: Baker Street</div>
+            // So that we can use user_selection[0] compare with the result from function CheckAnswer()
+            // to check if player selection is right or wrong
+            if (user_selection[0] == CheckAnswer()) {
+                // change answer color to green
+                $(CheckAnswer()).css('background-color', 'green').css('color', 'white');
 
 
-            // score function can be added score-- in here.
+                // score function can be added score++ in this if function
 
-        }
-        DisplayStats();
-        // when 3 attempts, game over, only allow to submit score.
-        if (attempt_counter <= 0) {
-            alert("GameOver!" + "Score shows up here");
-            $('#next').detach();
-            $('#submit').detach();
+            } else {
+                // change user_selection color to red, and answer to green
+                $(user_selection).css('color', 'red');
+                $(CheckAnswer()).css('background-color', 'green').css('color', 'white');
+
+                // attempt counter count one chance
+                attempt_counter--;
+
+
+                // score function can be added score-- in here.
+
+            }
+            DisplayStats();
+
+            // when 3 attempts, game over, only allow to submit score.
+            if (attempt_counter <= 0) {
+                alert("GameOver!");
+                $('#next').detach();
+                $('#submit').detach();
+                $('#cover-caption').slideToggle("slow");
+                $('#optionboard').hide()
+                $('#quit').hide()
+            }
         }
     }
 
     // function for checking which option is the answer
     function CheckAnswer() {
+
         //answer_location is getting from DisplayNewQuestion(), and the value is a int
         if (answer_location == 1) {
             //option_1 is getting from DisplayNewQuestion() option_1
             q_answer = option_1;
+
         } else if (answer_location == 2) {
             //option_2 is getting from DisplayNewQuestion() option_2
             q_answer = option_2;
+
         } else if (answer_location == 3) {
             //option_3 is getting from DisplayNewQuestion() option_3
             q_answer = option_3;
+
         } else {
             //option_4 is getting from DisplayNewQuestion() option_4
             q_answer = option_4;
