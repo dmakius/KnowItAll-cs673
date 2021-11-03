5a0a2e022dbcb0b8eb4c1a06302c0b990b7fd4b5
Daniel Makover <DanielM@centiro.se>
2021-11-03 14:33:36 +0100

    Models: Game and Player tables added

--- source/models.py
+++ source/models.py
@@ -1,4 +1,6 @@
 from . import db
+from sqlalchemy.sql import func
+from flask_login import UserMixin
 
 # Define question table Schema
 class Question(db.Model):
@@ -18,3 +20,27 @@ class LeaderboardScore(db.Model):
     category = db.Column(db.String(129), unique=False, nullable=False)
     username = db.Column(db.String(129), unique=False, nullable=False)
     score = db.Column(db.Integer)
+    
+# Define user table Schema
+class Player(db.Model, UserMixin):
+    __tablename__ = "player"
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(150), unique=True)
+    password = db.Column(db.String(150))
+    player_name = db.Column(db.String(150))
+    # TODO: add a relationship with the leaderboardScore using the foreign key.
+    # TODO: need to add and change some vairable.
+
+
+# Define Game table Schema
+# TODO Eventually this will need to be split between 'GameType' and 'GameSession' where 
+# 'GameType' defines initial variables and 'GameSession' stores the user's current game which updates during play
+class Game(db.Model):
+    __tablename__ = "Game"
+    id = db.Column(db.Integer, primary_key=True)
+    type = db.Column(db.String(129), unique=False, nullable=False)
+    lives = db.Column(db.Integer, unique=False, nullable = False)
+    score = db.Column(db.Integer, unique=False, nullable = False)
+    question_time = db.Column(db.Integer, unique=False, nullable = False)
+    skip_question = db.Column(db.Boolean, unique=False, nullable = False)
+    num_skip_question = db.Column(db.Integer, unique=False, nullable = False)
