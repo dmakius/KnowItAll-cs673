0f7d731308fc9ee124eb963580232134914a812b
Daniel Makover <DanielM@centiro.se>
2021-11-11 17:52:08 +0100

    tests added for category and game page

--- test/functional/test_routes.py
+++ test/functional/test_routes.py
@@ -15,7 +15,36 @@ def test_home_page():
         assert b"Player Profile" in response.data
         assert b"About" in response.data
 
-
-
+def test_category_page():
+    flask_app = create_app()
+    
+    with flask_app.test_client() as test_client:
+        response = test_client.get('/category')
+        assert response.status_code == 200
+        assert b"START" in response.data
+        assert b"GEOGRAPHY" in response.data
+        assert b"ART" in response.data
+        assert b"COMPUTER SCIENCE" in response.data
+        assert b"SCIENCE" in response.data
+        assert b"MYTHOLOGY" in response.data
+        assert b"TV SHOWS" in response.data
+        assert b"MOVIE" in response.data
+        assert b"ALL" in response.data
         
+def test_game_page():
+    flask_app = create_app()
+    
+    with flask_app.test_client() as test_client:
+        response = test_client.get('/game')
+        assert response.status_code == 200
+        assert b"LIVES" in response.data
+        assert b"SCORE" in response.data
+        assert b"TIME" in response.data
+        assert b"Lifelines" in response.data
+        assert b"Next Question" in response.data
+        assert b"Submit" in response.data
+        assert b"QUIT GAME" in response.data
+        assert b"Skip Question" in response.data
+        assert b"50/50" in response.data
         
+            
