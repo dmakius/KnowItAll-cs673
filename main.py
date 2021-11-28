3fb2d664426ef235733b6bc286bf62826c5d6d97
Daniel Makover <DanielM@centiro.se>
2021-11-29 00:39:27 +0100

    main file changed to PROD setting

--- main.py
+++ main.py
@@ -5,7 +5,5 @@ ENV = 'PROD'
 app = create_app(ENV)
 
 if __name__ == '__main__':
-    if ENV == "PROD":
-        app.run(debug=True)
-    else:
-        app.run(debug=False)
+    app.run(debug=True)
+   
