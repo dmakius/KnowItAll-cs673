621f6144500d7e71ba8546b5208d770615a74c30
Daniel Makover <DanielM@centiro.se>
2021-12-04 14:36:13 +0100

    fixed heroku admin create/disable files

--- source/helper_functions/prod_admin_create.py
+++ source/helper_functions/prod_admin_create.py
@@ -4,20 +4,21 @@ import sys
 user_email = ''
 
 if __name__ == "__main__":
-    if len(sys.argv) > 1:
-       user_email =sys.argv[1]
-
-if user_email  != "":
+    if len(sys.argv) > 0:
+        user_email =sys.argv[1]
+       
+if user_email != "":
     try:     
         db_connection_url = os.environ['DATABASE_URL'].replace("://", "ql://", 1)
         print('Connecting to the PostgreSQL database...')
         conn = psycopg2.connect(db_connection_url)
         cursor = conn.cursor()
-
-        cursor.execute('UPDATE "Player" SET admin = True WHERE email = %s' , (user_email))
+      
+        cursor.execute('UPDATE "Player" SET admin = TRUE WHERE email = %s' , (user_email,))
 
         conn.commit()
         cursor.close()
+        print("ADMIN enabled from: " + str(user_email))
     except (Exception, psycopg2.DatabaseError) as error:
         print("An Error has occurd!")
         print(error)
--- source/helper_functions/prod_admin_disable.py
+++ source/helper_functions/prod_admin_disable.py
@@ -4,20 +4,22 @@ import sys
 user_email = ''
 
 if __name__ == "__main__":
-    if len(sys.argv) > 1:
+    if len(sys.argv) > 0:
        user_email =sys.argv[1]
 
 if user_email  != "":
     try:     
         db_connection_url = os.environ['DATABASE_URL'].replace("://", "ql://", 1)
+
         print('Connecting to the PostgreSQL database...')
         conn = psycopg2.connect(db_connection_url)
         cursor = conn.cursor()
 
-        cursor.execute('UPDATE "Player" SET admin = False WHERE email = %s' , (user_email))
+        cursor.execute('UPDATE "Player" SET admin = False WHERE email = %s' , (user_email,))
 
         conn.commit()
         cursor.close()
+        print("ADMIN disabled from: " + str(user_email))
     except (Exception, psycopg2.DatabaseError) as error:
         print("An Error has occurd!")
         print(error)
