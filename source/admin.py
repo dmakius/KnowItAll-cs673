ed59a016856c26c060a8ec2e6fe1694816076cb7
Daniel Makover <DanielM@centiro.se>
2021-12-05 03:33:07 +0100

    fix: admin edit question

--- source/admin.py
+++ source/admin.py
@@ -45,13 +45,13 @@ def new():
 
 
 # route edit question
-@admin.route('admin/question/edit', methods=['GET', 'POST'])
+@admin.route('admin/question/edit', methods=['POST'])
 @login_required
 def edit():
     if is_super_user(current_user):
-        if request.method == 'POST':
-            id = request.form.get('question_id')
-            question = Question.query.filter_by(id=id).first()
+        id = request.form.get('question_id')
+        question = Question.query.filter_by(id=id).first()
+       
         return render_template('admin/edit_question.html', user=current_user, question=question)   
     else:
         flash('You are not allow to go to admin page.', category='error')
@@ -88,35 +88,22 @@ def add_question():
 
 
 # edit selected question
-@admin.route('admin/question/edit_question', methods=['GET', 'POST'])
+@admin.route('admin/question/edit_question', methods=['POST'])
 @login_required
 def edit_question():
-    if request.method == 'POST':
-        q_id = request.form.get('question_id')
-        q = Question.query.filter_by(id=q_id).first()
-
-        # checking which part of the question is editing
-        if request.form.get('category'):
-            new_category = request.form.get('category')
-            q.category = new_category
-        if request.form.get('question'):
-            new_question = request.form.get('question')
-            q.question = new_question
-        if request.form.get('answer'):
-            new_answer = request.form.get('answer')
-            q.answer = new_answer
-        if request.form.get('option_1'):
-            new_option1 = request.form.get('option_1')
-            q.option_1 = new_option1
-        if request.form.get('option_2'):
-            new_option2 = request.form.get('option_2')
-            q.option_2 = new_option2
-        if request.form.get('option_3'):
-            new_option3 = request.form.get('option_3')
-            q.option_3 = new_option3
-        db.session.commit()
-        flash('Edit success!', category='success')
-        return render_template("admin/edit_question.html", question=q, user=current_user)
+    print(request.form)
+    q_id = request.form.get('question_id')
+    q = Question.query.filter_by(id=q_id).first()
+    q.category = request.form.get('category')
+    q.question = request.form.get('question')
+    q.answer   = request.form.get('answer')
+    q.option_1 = request.form.get('option_1')
+    q.option_2 = request.form.get('option_2')
+    q.option_3 = request.form.get('option_3')
+           
+    db.session.commit()
+    flash('Question Editted Suceefully!', category='success')
+    return render_template("admin/edit_question.html", question=q, user=current_user)
 
 
 # delete selected question
